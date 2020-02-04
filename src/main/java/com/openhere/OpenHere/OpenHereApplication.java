f33adc4f34fe4c044e3471b28c5bd9eae7fbb629
nevzat.yavuzcan <nevzat.yavuzcan@etiya.com>
2020-02-04 10:44:20 +0300

    @RunSucce

--- src/main/java/com/openhere/OpenHere/OpenHereApplication.java
+++ src/main/java/com/openhere/OpenHere/OpenHereApplication.java
@@ -2,32 +2,36 @@ package com.openhere.OpenHere;
 
 import main.java.com.openhere.sahibinden.service.InquireKiralikDaire;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.event.EventListener;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.jms.annotation.EnableJms;
 
-@Configuration
-@EnableAutoConfiguration
-@ComponentScan
+import javax.annotation.PostConstruct;
+
+@SpringBootApplication
+@ComponentScan("main.java.com.openhere")
+@EnableJms
 public class OpenHereApplication extends SpringBootServletInitializer {
-	@SuppressWarnings("resource")
-	public static void main(String[] args) throws Exception {
-		ConfigurableApplicationContext context = SpringApplication.run(OpenHereApplication.class, args);
-		InquireKiralikDaire inquireKiralikDaire =	context.getBean(InquireKiralikDaire.class);
 
+	@Autowired
+	private InquireKiralikDaire inquireKiralikDaire;
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(OpenHereApplication.class);
 	}
 
-	@EventListener(ApplicationReadyEvent.class)
-	public void doSomethingAfterStartup() {
-
+	@PostConstruct
+	public void listen() throws Exception {
+		inquireKiralikDaire.inquireDaireler();
 	}
 
+	public static void main(String[] args) {
+		ApplicationContext context = SpringApplication.run(OpenHereApplication.class, args);
+		//LogService.info(Application.class.getName(), "Service Started...");
+	}
 }
